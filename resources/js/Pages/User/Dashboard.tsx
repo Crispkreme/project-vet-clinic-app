4dc149dd8c13828db5c0be81edbb009f88506575
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-10-12 13:38:58 +0800

    add routes
    added routes and updating the admin and the use page

--- resources/js/Pages/User/Dashboard.tsx
+++ resources/js/Pages/User/Dashboard.tsx
@@ -1,7 +1,6 @@
 import MainContent from '@/Components/MainContent';
 import UserProfile from '@/Components/Profile/UserProfile';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
 
 export default function Dashboard() {
     return (
